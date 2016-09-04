98f10527470b35eef4a894d8a4b7b105b23bcfad
crosspitter <dhiva33@gmail.com>
2016-09-04 11:17:07 -0700

    enum modified

--- src/com/dhiva/server/HttpRequest.java
+++ src/com/dhiva/server/HttpRequest.java
@@ -1,16 +1,33 @@
 package com.dhiva.server;
 
 public class HttpRequest {
-	private HttpMethod httpMethod;
 	private String resourceURI;
 	private String httpVersion;
 
 	public enum HttpMethod {
-		GET("GET"),
-		HEAD, POST, PUT, DELETE, TRACE, CONNECT, BAD
-	};
+		GET("GET"), POST("POST"), PUT("PUT"), DELETE("DELETE"), BAD("BAD"), HEAD("HEAD");
 
+		private String httpMethod;
 
+		HttpMethod(String requestType) {
+			this.httpMethod = requestType;
+		}
+
+		public String getHttpMethod() {
+			return httpMethod.toString();
+		}
+
+		public static HttpMethod findHttpMethod(String text) {
+			if (text != null) {
+				for (HttpMethod b : HttpMethod.values()) {
+					if (text.equalsIgnoreCase(b.httpMethod)) {
+						return b;
+					}
+				}
+			}
+			return null;
+		}
+	}
 
 	public void setHttpVersion(String requestVersion) {
 		this.httpVersion = requestVersion;
@@ -20,31 +37,6 @@ public class HttpRequest {
 		this.resourceURI = requestFile;
 	}
 
-	public void setHttpMethod(String requestType) {
-		String httpMethod = requestType;
-		if (httpMethod.equals("GET")) {
-			this.httpMethod = HttpMethod.GET;
-		} else if (httpMethod.equals("HEAD")) {
-			this.httpMethod = HttpMethod.HEAD;
-		} else if (httpMethod.equals("POST")) {
-			this.httpMethod = HttpMethod.POST;
-		} else if (httpMethod.equals("PUT")) {
-			this.httpMethod = HttpMethod.PUT;
-		} else if (httpMethod.equals("DELETE")) {
-			this.httpMethod = HttpMethod.DELETE;
-		} else if (httpMethod.equals("TRACE")) {
-			this.httpMethod = HttpMethod.TRACE;
-		} else if (httpMethod.equals("CONNECT")) {
-			this.httpMethod = HttpMethod.CONNECT;
-		} else {
-			this.httpMethod = HttpMethod.BAD;
-		}
-	}
-	
-	public String getHttpMethod() {
-		return httpMethod.toString();
-	}
-
 	public String getResourceURI() {
 		return resourceURI;
 	}
@@ -52,6 +44,5 @@ public class HttpRequest {
 	public String getHttpVersion() {
 		return httpVersion;
 	}
-
 
 }
