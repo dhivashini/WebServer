af357036575fbc16847730fbec15383f06351c6a
crosspitter <dhiva33@gmail.com>
2016-09-07 19:25:23 -0700

    updated enum

--- src/com/dhiva/server/HttpRequest.java
+++ src/com/dhiva/server/HttpRequest.java
@@ -5,49 +5,30 @@ public class HttpRequest {
 	private String httpVersion;
 	private HttpMethod httpMethod;
 
-	enum HttpMethod {
-		GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT, BAD
-	};
+	public enum HttpMethod {
+		GET("GET"), POST("POST"), PUT("PUT"), DELETE("DELETE"), BAD("BAD"), HEAD("HEAD");
+
+		private String httpMethod;
+
+		HttpMethod(String requestType) {
+			this.httpMethod = requestType;
+		}
+	}
 
 	public void setHttpMethod(String requestType) {
-		String httpMethod = requestType;
-		if (httpMethod.equals("GET")) {
-			this.httpMethod = HttpMethod.GET;
-		} else if (httpMethod.equals("HEAD")) {
-			this.httpMethod = HttpMethod.HEAD;
-		} else if (httpMethod.equals("POST")) {
-			this.httpMethod = HttpMethod.POST;
-		} else if (httpMethod.equals("PUT")) {
-			this.httpMethod = HttpMethod.PUT;
-		} else if (httpMethod.equals("DELETE")) {
-			this.httpMethod = HttpMethod.DELETE;
-		} else if (httpMethod.equals("TRACE")) {
-			this.httpMethod = HttpMethod.TRACE;
-		} else if (httpMethod.equals("CONNECT")) {
-			this.httpMethod = HttpMethod.CONNECT;
-		} else {
-			this.httpMethod = HttpMethod.BAD;
+		if (requestType != null) {
+			for (HttpMethod b : HttpMethod.values()) {
+				if (requestType.equalsIgnoreCase(b.httpMethod)) {
+					this.httpMethod = b;
+
+				}
+			}
 		}
 	}
 
 	public String getHttpMethod() {
 		return httpMethod.toString();
 	}
-
-	/*
-	 * public enum HttpMethod { GET("GET"), POST("POST"), PUT("PUT"),
-	 * DELETE("DELETE"), BAD("BAD"), HEAD("HEAD");
-	 * 
-	 * private String httpMethod;
-	 * 
-	 * HttpMethod(String requestType) { this.httpMethod = requestType; }
-	 * 
-	 * public String getHttpMethod() { return httpMethod.toString(); }
-	 * 
-	 * public HttpMethod setHttpMethod(String text) { if (text != null) { for
-	 * (HttpMethod b : HttpMethod.values()) { if
-	 * (text.equalsIgnoreCase(b.httpMethod)) { return b; } } } return null; } }
-	 */
 
 	public void setHttpVersion(String requestVersion) {
 		this.httpVersion = requestVersion;
